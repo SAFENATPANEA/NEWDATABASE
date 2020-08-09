fd8e459204e95467bb8d655ba5537234538fe9e9
SAFENATPANEA <SAFENAT@OUTLOOK.COM>
2020-08-09 03:34:45 -0400

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace organigramaWPF
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Congre { get; set; }
+
+        public string Departamento { get; set; }
+
+        public string Telf { get; set; }
+
+        public string Correo { get; set; }
+
+        public DateTime Birthday { get; set; }
+
+        public string Details
+        {
+            get
+            {
+                return String.Format("{0} was born on {1} and this is a long description of the person.", this.Name, this.Birthday.ToLongDateString());
+            }
+        }
+    }
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+       
+        public MainWindow()
+        {
+            InitializeComponent();         
+            
+
+        }
+
+        private void ListViewItem_MouseEnter(object sender, MouseEventArgs e)
+        {
+            if (tg_btn.IsChecked == true)
+            {
+                tt_home.Visibility = Visibility.Collapsed;
+                tt_contact.Visibility = Visibility.Collapsed;
+                tt_mensaje.Visibility = Visibility.Collapsed;
+                tt_Maps.Visibility = Visibility.Collapsed;
+                tt_Settings.Visibility = Visibility.Collapsed;
+                tt_so.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                tt_home.Visibility = Visibility.Visible;
+                tt_contact.Visibility = Visibility.Visible;
+                tt_mensaje.Visibility = Visibility.Visible;
+                tt_Maps.Visibility = Visibility.Visible;
+                tt_Settings.Visibility = Visibility.Visible;
+                tt_so.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void BG_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            tg_btn.IsChecked = false;
+        }
+
+        private void CloseBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Tg_btn_Checked(object sender, RoutedEventArgs e)
+        {
+            img_bg.Opacity = 0.3;
+        }
+
+        private void Tg_btn_Unchecked(object sender, RoutedEventArgs e)
+        {
+            img_bg.Opacity = 1;
+        }
+    }
+}
